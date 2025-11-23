4d482b4802e055573a2a79965c937b9bcad8e6b1
iansaini15 <121358305+iansaini15@users.noreply.github.com>
2025-11-23 04:37:19 +0000

    Remove old demotest.js file after migration to demo.test.js

--- tests/demotest.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Title matches expected value', async ({ page }) => {
-  await page.goto('https://iansaini.com');
-  await expect(page).toHaveTitle('Ian Saini | Home');
-
-const footer = page.locator('div');
-  await expect(footer).toBeVisible();
-});
